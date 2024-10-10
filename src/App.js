2c9bb8974f1594ebacda30f15504036e22b68ae4
DeepakSarun <deepaksarun.yuvachandran@slu.edu>
2024-10-10 11:00:32 -0500

    initial submission

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './Team8.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Website Loading...
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
